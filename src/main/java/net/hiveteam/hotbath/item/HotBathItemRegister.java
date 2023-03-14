b1164046fc8fb7790a21dda428f7fec51a8750d8
Alex <seiu255522@gmail.com>
2023-03-14 18:55:42 -0400

    add Peony Bath Bucket

--- src/main/java/net/hiveteam/hotbath/item/HotBathItemRegister.java
+++ src/main/java/net/hiveteam/hotbath/item/HotBathItemRegister.java
@@ -12,9 +12,7 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class HotBathItemRegister {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, HotBath.MOD_ID);
-
-    public static final RegistryObject<Item> HOT_WATER_BUCKET = ITEMS.register("hot_water_bucket",
-            () -> new BucketItem(() -> HotBathFluidsRegister.HOT_WATER_FLUID.get(), new Item.Properties().group(HotBathItemGroup.HOT_BATH)));
+    // Write by alphabet order please!
 
     public static final RegistryObject<Item> HERBAL_BATH_BUCKET = ITEMS.register("herbal_bath_bucket",
             () -> new Item(new Item.Properties().group(HotBathItemGroup.HOT_BATH)));
@@ -25,13 +23,19 @@ public class HotBathItemRegister {
     public static final RegistryObject<Item> MILK_BATH_BUCKET = ITEMS.register("milk_bath_bucket",
             () -> new Item(new Item.Properties().group(HotBathItemGroup.HOT_BATH)));
 
+    public static final RegistryObject<Item> PEONY_BATH_BUCKET = ITEMS.register("peony_bath_bucket",
+            () -> new Item(new Item.Properties().group(HotBathItemGroup.HOT_BATH)));
+
     public static final RegistryObject<Item> ROSE_BATH_BUCKET = ITEMS.register("rose_bath_bucket",
             () -> new Item(new Item.Properties().group(HotBathItemGroup.HOT_BATH)));
 
+
+    public static final RegistryObject<Item> HOT_WATER_BUCKET = ITEMS.register("hot_water_bucket",
+            () -> new BucketItem(() -> HotBathFluidsRegister.HOT_WATER_FLUID.get(), new Item.Properties().group(HotBathItemGroup.HOT_BATH)));
+
+
     public static final RegistryObject<Item> BATH_HERB = ITEMS.register("bath_herb",
             () -> new Item(new Item.Properties().group(HotBathItemGroup.HOT_BATH)));
-
-
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
     }

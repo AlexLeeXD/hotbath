edf493607bbcc8feb29fb1a28c4ee4fe64963b2c
Alex Li <islicy@outlook.com>
2023-03-16 14:47:53 -0400

    set fluids color

--- src/main/java/net/hiveteam/hotbath/fluid_details/FluidsColor.java
+++ src/main/java/net/hiveteam/hotbath/fluid_details/FluidsColor.java
@@ -6,11 +6,11 @@ public final class FluidsColor {
     return 0xff000000 | (red << 16) | (green << 8) | blue;
   }
 
-  public static final int HERBAL_BATH_COLOR = getFluidColor(255, 255, 255);
-  public static final int HOT_WATER_COLOR = getFluidColor(255, 255, 0);
-  public static final int HONEY_BATH_COLOR = getFluidColor(255, 255, 255);
+  public static final int HERBAL_BATH_COLOR = getFluidColor(36, 202, 173);
+  public static final int HOT_WATER_COLOR = getFluidColor(69, 225, 233);
+  public static final int HONEY_BATH_COLOR = getFluidColor(254, 209, 122);
 
-  public static final int MILK_BATH_COLOR = getFluidColor(255, 255, 255);
-  public static final int PEONY_BATH_COLOR = getFluidColor(255, 255, 255);
-  public static final int ROSE_BATH_COLOR = getFluidColor(255, 255, 255);
+  public static final int MILK_BATH_COLOR = getFluidColor(248, 248, 248);
+  public static final int PEONY_BATH_COLOR = getFluidColor(253, 104, 159);
+  public static final int ROSE_BATH_COLOR = getFluidColor(254, 119, 142);
 }

c916b61cb2ddd4a59087a9f896f118037e248857
sigmundscarlet <1450291012@qq.com>
2023-03-20 23:51:50 +0800

    新增检测实体在热水里的发方法

--- src/main/java/net/hiveteam/hotbath/fluid_blocks/HotWaterBlock.java
+++ src/main/java/net/hiveteam/hotbath/fluid_blocks/HotWaterBlock.java
@@ -5,6 +5,8 @@ import static net.hiveteam.hotbath.util.ParticleGenerator.renderDefaultSteam;
 import java.util.function.Supplier;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.FlowingFluidBlock;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.fluid.FlowingFluid;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -18,5 +20,13 @@ public class HotWaterBlock extends FlowingFluidBlock {
   @Override
   public void animateTick(BlockState stateIn, World worldIn, BlockPos pos, java.util.Random rand) {
     renderDefaultSteam(worldIn, pos, rand);
+  }
+
+  @Override
+  public void onEntityCollision(BlockState state, World worldIn, BlockPos pos, Entity entityIn) {
+    if( entityIn instanceof PlayerEntity){
+      PlayerEntity player = (PlayerEntity)entityIn;
+      LOGGER.info(player.getUniqueID());
+    }
   }
 }

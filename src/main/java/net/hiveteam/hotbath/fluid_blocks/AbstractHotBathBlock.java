516bf22f0a45fa7b76c0307dd058d6507edc9ea6
Alex Li <islicy@outlook.com>
2023-04-05 15:25:58 -0400

    fix advancements bugs

--- src/main/java/net/hiveteam/hotbath/fluid_blocks/AbstractHotBathBlock.java
+++ src/main/java/net/hiveteam/hotbath/fluid_blocks/AbstractHotBathBlock.java
@@ -1,5 +1,8 @@
 package net.hiveteam.hotbath.fluid_blocks;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Supplier;
 import net.hiveteam.hotbath.otherDomain.AttributeParameter;
 import net.hiveteam.hotbath.otherDomain.EffectParameter;
 import net.hiveteam.hotbath.util.EffectChangeUtil;
@@ -15,85 +18,102 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.Supplier;
-
 public abstract class AbstractHotBathBlock extends FlowingFluidBlock {
-    public static final int TICK_NUMBER = 20;
-    public long lastTick = 0;
-    public Integer stayTime;// 在液体里呆的时间
-    public String stayTimeDataId;
-    public Integer enteredCount;// 在液体里呆的时间
-    public String enteredCountDataId;
-    public String advancementId;
-    public List<AttributeParameter> attributeParameterList = new ArrayList<>();
-    public List<EffectParameter> effectParameterList = new ArrayList<>();
+  public static final int TICK_NUMBER = 20;
+  public long lastTick = 0;
+  public Integer stayTime; // 在液体里呆的时间
+  public String stayTimeDataId;
+  public Integer enteredCount; // 在液体里呆的时间
+  public String enteredCountDataId;
+  public String advancementId;
+  public List<AttributeParameter> attributeParameterList = new ArrayList<>();
+  public List<EffectParameter> effectParameterList = new ArrayList<>();
 
-    public AbstractHotBathBlock(Supplier<? extends FlowingFluid> supplier, Properties properties) {
-        super(supplier, properties);
+  public AbstractHotBathBlock(Supplier<? extends FlowingFluid> supplier, Properties properties) {
+    super(supplier, properties);
+  }
+
+  public static void enterHotBathEvents(
+      ServerPlayerEntity serverPlayerEntity,
+      EffectParameter effectParameter,
+      AttributeParameter attributeParameter,
+      String playerDataId) {
+    CompoundNBT playerData = serverPlayerEntity.getPersistentData();
+    if (effectParameter != null) {
+      if (playerData.getInt(playerDataId) >= effectParameter.stayTime * TICK_NUMBER) {
+        serverPlayerEntity.addPotionEffect(
+            new EffectInstance(
+                effectParameter.effect,
+                TICK_NUMBER * effectParameter.effectTime,
+                effectParameter.amplifierIn,
+                false,
+                false,
+                true));
+      }
     }
+    if (attributeParameter != null) {
+      if (playerData.getInt(playerDataId) >= attributeParameter.stayTime * TICK_NUMBER) {
 
-    public static void enterHotBathEvents(ServerPlayerEntity serverPlayerEntity, EffectParameter effectParameter, AttributeParameter attributeParameter, String playerDataId) {
-        CompoundNBT playerData = serverPlayerEntity.getPersistentData();
-        if (effectParameter != null) {
-            if (playerData.getInt(playerDataId) >= effectParameter.stayTime * TICK_NUMBER) {
-                serverPlayerEntity.addPotionEffect(
-                        new EffectInstance(effectParameter.effect, TICK_NUMBER * effectParameter.effectTime, effectParameter.amplifierIn, false, false, true));
-            }
-        }
-        if (attributeParameter != null) {
-            if (playerData.getInt(playerDataId) >= attributeParameter.stayTime * TICK_NUMBER) {
-
-                EffectChangeUtil.applyAttributeModifier(
-                        serverPlayerEntity,
-                        attributeParameter.attribute, attributeParameter.modifier);
-            }
-        }
+        EffectChangeUtil.applyAttributeModifier(
+            serverPlayerEntity, attributeParameter.attribute, attributeParameter.modifier);
+      }
     }
+  }
 
-    public static void enterHotBathAdancements(ServerPlayerEntity serverPlayerEntity, Integer enteredCount, String playerDataId, String AdvancementId) {
-        CompoundNBT playerData = serverPlayerEntity.getPersistentData();
-        playerData.putInt(playerDataId, playerData.getInt(playerDataId) + 1);
+  public static void enterHotBathAdancements(
+      ServerPlayerEntity serverPlayerEntity,
+      Integer enteredCount,
+      String enteredCountDataId,
+      String AdvancementId) {
+    CompoundNBT playerData = serverPlayerEntity.getPersistentData();
 
-        if (playerData.getInt(playerDataId) >= enteredCount) {
-            Advancement advancement =
-                    serverPlayerEntity
-                            .getServer()
-                            .getAdvancementManager()
-                            .getAdvancement(ResourceLocation.tryCreate(AdvancementId));
+    if (playerData.getInt(enteredCountDataId) >= enteredCount) {
+      Advancement advancement =
+          serverPlayerEntity
+              .getServer()
+              .getAdvancementManager()
+              .getAdvancement(ResourceLocation.tryCreate(AdvancementId));
 
-            if (advancement != null) {
-                serverPlayerEntity.getAdvancements().grantCriterion(advancement, "code_triggered");
-                playerData.putInt(playerDataId, 0);
-            }
-        }
+      if (advancement != null) {
+        serverPlayerEntity.getAdvancements().grantCriterion(advancement, "code_triggered");
+        playerData.putInt(enteredCountDataId, 0);
+      }
     }
+  }
 
-    @Override
-    public void onEntityCollision(BlockState state, World worldIn, BlockPos pos, Entity entityIn) {
+  @Override
+  public void onEntityCollision(BlockState state, World worldIn, BlockPos pos, Entity entityIn) {
 
-        if (entityIn instanceof ServerPlayerEntity) {
-            ServerPlayerEntity serverPlayerEntity = (ServerPlayerEntity) entityIn;
-            CompoundNBT playerData = serverPlayerEntity.getPersistentData();
-            if (lastTick + 5 <= worldIn.getGameTime()) {
-                playerData.putInt(stayTimeDataId, 0);
-                playerData.putInt(enteredCountDataId, playerData.getInt(enteredCountDataId) + 1);
-            }
-            lastTick = worldIn.getGameTime();
-            playerData.putInt(stayTimeDataId, playerData.getInt(stayTimeDataId) + 1);
-            //此处偷懒
-            for(EffectParameter effectParameter:effectParameterList){
-                enterHotBathEvents(serverPlayerEntity, effectParameter,null, stayTimeDataId);
-            }
-            for(AttributeParameter attributeParameter:attributeParameterList){
-                enterHotBathEvents(serverPlayerEntity,null, attributeParameter, stayTimeDataId);
-            }
+    if (entityIn instanceof ServerPlayerEntity) {
+      ServerPlayerEntity serverPlayerEntity = (ServerPlayerEntity) entityIn;
+      CompoundNBT playerData = serverPlayerEntity.getPersistentData();
+      if (lastTick + 5 <= worldIn.getGameTime()) {
+        LOGGER.info("lastTick: " + lastTick);
+        LOGGER.info("worldIn.getGameTime(): " + worldIn.getGameTime());
 
-            specialEffect(serverPlayerEntity, playerData.getInt(stayTimeDataId), playerData.getInt(enteredCountDataId));
-            enterHotBathAdancements(serverPlayerEntity, enteredCount, enteredCountDataId, advancementId);
-        }
+        playerData.putInt(stayTimeDataId, 0);
+        playerData.putInt(enteredCountDataId, playerData.getInt(enteredCountDataId) + 1);
+      }
+      lastTick = worldIn.getGameTime();
+      playerData.putInt(stayTimeDataId, playerData.getInt(stayTimeDataId) + 1);
+      // 此处偷懒
+      for (EffectParameter effectParameter : effectParameterList) {
+        enterHotBathEvents(serverPlayerEntity, effectParameter, null, stayTimeDataId);
+      }
+      for (AttributeParameter attributeParameter : attributeParameterList) {
+        enterHotBathEvents(serverPlayerEntity, null, attributeParameter, stayTimeDataId);
+      }
+
+      specialEffect(
+          serverPlayerEntity,
+          playerData.getInt(stayTimeDataId),
+          playerData.getInt(enteredCountDataId));
+      enterHotBathAdancements(serverPlayerEntity, enteredCount, enteredCountDataId, advancementId);
+
+      LOGGER.info("enteredCount: " + playerData.getInt(enteredCountDataId));
     }
+  }
 
-    abstract public void specialEffect(ServerPlayerEntity serverPlayerEntity, Integer stayTime, Integer enteredCount);
+  public abstract void specialEffect(
+      ServerPlayerEntity serverPlayerEntity, Integer stayTime, Integer enteredCount);
 }
--- src/main/java/net/hiveteam/hotbath/fluid_blocks/HotWaterBlock.java
+++ src/main/java/net/hiveteam/hotbath/fluid_blocks/HotWaterBlock.java
@@ -4,7 +4,6 @@ import static net.hiveteam.hotbath.util.ParticleGenerator.renderDefaultSteam;
 
 import java.util.UUID;
 import java.util.function.Supplier;
-
 import net.hiveteam.hotbath.otherDomain.AttributeParameter;
 import net.hiveteam.hotbath.otherDomain.EffectParameter;
 import net.minecraft.block.BlockState;
@@ -16,35 +15,33 @@ import net.minecraft.potion.Effects;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
-/**
- * Hot Water Block
- */
+/** Hot Water Block */
 public class HotWaterBlock extends AbstractHotBathBlock {
 
-    public HotWaterBlock(Supplier<? extends FlowingFluid> supplier, Properties properties) {
-        super(supplier, properties);
-        stayTimeDataId="HotWaterStayedTime";
-        enteredCountDataId ="HotWaterEnteredNumber";
-        advancementId ="hotbath:foot_health";
-        enteredCount = 100;
-        AttributeParameter attributeParameter = new AttributeParameter(Attributes.ATTACK_SPEED,
-                UUID.fromString("3e3b0f20-7a04-11ec-9621-0242ac130003"),
-                0.10,
-                AttributeModifier.Operation.MULTIPLY_TOTAL,5);
-        EffectParameter effectParameter = new EffectParameter(20, Effects.SPEED, 1,15);
-        attributeParameterList.add(attributeParameter);
-        effectParameterList.add(effectParameter);
-    }
+  public HotWaterBlock(Supplier<? extends FlowingFluid> supplier, Properties properties) {
+    super(supplier, properties);
+    stayTimeDataId = "HotWaterStayedTime";
+    enteredCountDataId = "HotWaterEnteredNumber";
+    advancementId = "hotbath:foot_health";
+    enteredCount = 100;
+    AttributeParameter attributeParameter =
+        new AttributeParameter(
+            Attributes.ATTACK_SPEED,
+            UUID.fromString("3e3b0f20-7a04-11ec-9621-0242ac130003"),
+            0.10,
+            AttributeModifier.Operation.MULTIPLY_TOTAL,
+            5);
+    EffectParameter effectParameter = new EffectParameter(20, Effects.SPEED, 0, 15);
+    attributeParameterList.add(attributeParameter);
+    effectParameterList.add(effectParameter);
+  }
 
-    @Override
-    public void specialEffect(ServerPlayerEntity serverPlayerEntity, Integer stayTime,Integer enteredCount) {
+  @Override
+  public void specialEffect(
+      ServerPlayerEntity serverPlayerEntity, Integer stayTime, Integer enteredCount) {}
 
-    }
-
-    @Override
-    public void animateTick(BlockState stateIn, World worldIn, BlockPos pos, java.util.Random rand) {
-        renderDefaultSteam(worldIn, pos, rand);
-    }
-
-
+  @Override
+  public void animateTick(BlockState stateIn, World worldIn, BlockPos pos, java.util.Random rand) {
+    renderDefaultSteam(worldIn, pos, rand);
+  }
 }

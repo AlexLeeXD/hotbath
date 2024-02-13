1b397e76dcf34267e20066339f0e7a8e1c4aa6db
Alex Lee <islicy@outlook.com>
2024-02-13 01:36:19 -0500

    Fix inBlock logic

--- src/main/java/hivestandsteam/hotbath/util/CustomFluidHandler.java
+++ src/main/java/hivestandsteam/hotbath/util/CustomFluidHandler.java
@@ -1,8 +1,7 @@
 package hivestandsteam.hotbath.util;
 
 import hivestandsteam.hotbath.HotBath;
-import hivestandsteam.hotbath.fluid_blocks.IHotbathBlock;
-import net.minecraft.block.Block;
+import hivestandsteam.hotbath.fluid_blocks.*;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -21,49 +20,42 @@ public class CustomFluidHandler {
   public static boolean isPlayerInHotWaterBlock(PlayerEntity player) {
     BlockPos playerPos = player.getPosition();
     BlockState stateAtPlayerPos = player.world.getBlockState(playerPos);
-    Block specificBlock = HotBath.HOT_WATER_BLOCK;
-    return stateAtPlayerPos.getBlock() == specificBlock;
+    return stateAtPlayerPos.getBlock() instanceof IHotbathBlock;
   }
 
   public static boolean isPlayerInHoneyBathBlock(PlayerEntity player) {
     BlockPos playerPos = player.getPosition();
     BlockState stateAtPlayerPos = player.world.getBlockState(playerPos);
-    Block specificBlock = HotBath.HONEY_BATH_BLOCK;
-    return stateAtPlayerPos.getBlock() == specificBlock;
+    return stateAtPlayerPos.getBlock() instanceof HoneyBathBlock;
   }
 
   public static boolean isPlayerInPeonyBathBlock(PlayerEntity player) {
     BlockPos playerPos = player.getPosition();
     BlockState stateAtPlayerPos = player.world.getBlockState(playerPos);
-    Block specificBlock = HotBath.PEONY_BATH_BLOCK;
-    return stateAtPlayerPos.getBlock() == specificBlock;
+    return stateAtPlayerPos.getBlock() instanceof PeonyBathBlock;
   }
 
   public static boolean isPlayerInMilkBathBlock(PlayerEntity player) {
     BlockPos playerPos = player.getPosition();
     BlockState stateAtPlayerPos = player.world.getBlockState(playerPos);
-    Block specificBlock = HotBath.MILK_BATH_BLOCK;
-    return stateAtPlayerPos.getBlock() == specificBlock;
+    return stateAtPlayerPos.getBlock() instanceof MilkBathBlock;
   }
 
   public static boolean isPlayerInHerbalBathBlock(PlayerEntity player) {
     BlockPos playerPos = player.getPosition();
     BlockState stateAtPlayerPos = player.world.getBlockState(playerPos);
-    Block specificBlock = HotBath.HERBAL_BATH_BLOCK;
-    return stateAtPlayerPos.getBlock() == specificBlock;
+    return stateAtPlayerPos.getBlock() instanceof HerbalBathBlock;
   }
 
   public static boolean isEntityInHerbalBathBlock(Entity entity) {
     BlockPos entityPos = entity.getPosition();
     BlockState stateAtEntityPos = entity.world.getBlockState(entityPos);
-    Block specificBlock = HotBath.HERBAL_BATH_BLOCK;
-    return stateAtEntityPos.getBlock() == specificBlock;
+    return stateAtEntityPos.getBlock() instanceof HerbalBathBlock;
   }
 
   public static boolean isPlayerInRoseBathBlock(PlayerEntity player) {
     BlockPos playerPos = player.getPosition();
     BlockState stateAtPlayerPos = player.world.getBlockState(playerPos);
-    Block specificBlock = HotBath.ROSE_BATH_BLOCK;
-    return stateAtPlayerPos.getBlock() == specificBlock;
+    return stateAtPlayerPos.getBlock() instanceof RoseBathBlock;
   }
 }

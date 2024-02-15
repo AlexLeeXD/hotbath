916abe135cbbef16ce4fe3749b43da4bafcfa0d3
Alex Lee <islicy@outlook.com>
2024-02-14 19:40:45 -0500

    Add register

--- src/main/java/com/crabmod/hotbath/HotBath.java
+++ src/main/java/com/crabmod/hotbath/HotBath.java
@@ -1,5 +1,9 @@
 package com.crabmod.hotbath;
 
+import com.crabmod.hotbath.registers.BlocksRegister;
+import com.crabmod.hotbath.registers.FluidsRegister;
+import com.crabmod.hotbath.registers.ItemRegister;
+import com.crabmod.hotbath.registers.ParticleRegister;
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.world.item.BlockItem;
@@ -25,73 +29,80 @@ import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(HotBath.MODID)
-public class HotBath
-{
-    // Define mod id in a common place for everything to reference
-    public static final String MODID = "hotbath";
-    // Directly reference a slf4j logger
-    private static final Logger LOGGER = LogUtils.getLogger();
-    // Create a Deferred Register to hold Blocks which will all be registered under the "hotbath" namespace
-    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
-    // Create a Deferred Register to hold Items which will all be registered under the "hotbath" namespace
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
+@Mod(HotBath.MOD_ID)
+public class HotBath {
+  // Define mod id in a common place for everything to reference
+  public static final String MOD_ID = "hotbath";
+  // Directly reference a slf4j logger
+  private static final Logger LOGGER = LogUtils.getLogger();
+  // Create a Deferred Register to hold Blocks which will all be registered under the "hotbath"
+  // namespace
+  public static final DeferredRegister<Block> BLOCKS =
+      DeferredRegister.create(ForgeRegistries.BLOCKS, MOD_ID);
+  // Create a Deferred Register to hold Items which will all be registered under the "hotbath"
+  // namespace
+  public static final DeferredRegister<Item> ITEMS =
+      DeferredRegister.create(ForgeRegistries.ITEMS, MOD_ID);
 
-    // Creates a new Block with the id "hotbath:example_block", combining the namespace and path
-    public static final RegistryObject<Block> EXAMPLE_BLOCK = BLOCKS.register("example_block", () -> new Block(BlockBehaviour.Properties.of(Material.STONE)));
-    // Creates a new BlockItem with the id "hotbath:example_block", combining the namespace and path
-    public static final RegistryObject<Item> EXAMPLE_BLOCK_ITEM = ITEMS.register("example_block", () -> new BlockItem(EXAMPLE_BLOCK.get(), new Item.Properties()));
+  // Creates a new Block with the id "hotbath:example_block", combining the namespace and path
+  public static final RegistryObject<Block> EXAMPLE_BLOCK =
+      BLOCKS.register(
+          "example_block", () -> new Block(BlockBehaviour.Properties.of(Material.STONE)));
+  // Creates a new BlockItem with the id "hotbath:example_block", combining the namespace and path
+  public static final RegistryObject<Item> EXAMPLE_BLOCK_ITEM =
+      ITEMS.register(
+          "example_block", () -> new BlockItem(EXAMPLE_BLOCK.get(), new Item.Properties()));
 
-    public HotBath()
-    {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+  public HotBath() {
+    IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        // Register the commonSetup method for modloading
-        modEventBus.addListener(this::commonSetup);
+    modEventBus.addListener(this::commonSetup);
 
-        // Register the Deferred Register to the mod event bus so blocks get registered
-        BLOCKS.register(modEventBus);
-        // Register the Deferred Register to the mod event bus so items get registered
-        ITEMS.register(modEventBus);
+    FluidsRegister.register(modEventBus);
+    BlocksRegister.register(modEventBus);
+    ItemRegister.register(modEventBus);
+    ParticleRegister.register(modEventBus);
+    // Register the commonSetup method for modloading
+    modEventBus.addListener(this::commonSetup);
 
-        // Register ourselves for server and other game events we are interested in
-        MinecraftForge.EVENT_BUS.register(this);
+    // Register the Deferred Register to the mod event bus so blocks get registered
+    BLOCKS.register(modEventBus);
+    // Register the Deferred Register to the mod event bus so items get registered
+    ITEMS.register(modEventBus);
 
-        // Register the item to a creative tab
-        modEventBus.addListener(this::addCreative);
-    }
+    // Register ourselves for server and other game events we are interested in
+    MinecraftForge.EVENT_BUS.register(this);
 
-    private void commonSetup(final FMLCommonSetupEvent event)
-    {
-        // Some common setup code
-        LOGGER.info("HELLO FROM COMMON SETUP");
-        LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
-    }
+    // Register the item to a creative tab
+    modEventBus.addListener(this::addCreative);
+  }
 
-    private void addCreative(CreativeModeTabEvent.BuildContents event)
-    {
-        if (event.getTab() == CreativeModeTabs.BUILDING_BLOCKS)
-            event.accept(EXAMPLE_BLOCK_ITEM);
-    }
+  private void commonSetup(final FMLCommonSetupEvent event) {
+    // Some common setup code
+    LOGGER.info("HELLO FROM COMMON SETUP");
+    LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
+  }
 
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
+  private void addCreative(CreativeModeTabEvent.BuildContents event) {
+    if (event.getTab() == CreativeModeTabs.BUILDING_BLOCKS) event.accept(EXAMPLE_BLOCK_ITEM);
+  }
+
+  // You can use SubscribeEvent and let the Event Bus discover methods to call
+  @SubscribeEvent
+  public void onServerStarting(ServerStartingEvent event) {
+    // Do something when the server starts
+    LOGGER.info("HELLO from server starting");
+  }
+
+  // You can use EventBusSubscriber to automatically register all static methods in the class
+  // annotated with @SubscribeEvent
+  @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+  public static class ClientModEvents {
     @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event)
-    {
-        // Do something when the server starts
-        LOGGER.info("HELLO from server starting");
+    public static void onClientSetup(FMLClientSetupEvent event) {
+      // Some client setup code
+      LOGGER.info("HELLO FROM CLIENT SETUP");
+      LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
     }
-
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents
-    {
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event)
-        {
-            // Some client setup code
-            LOGGER.info("HELLO FROM CLIENT SETUP");
-            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
-        }
-    }
+  }
 }
---
+++ src/main/java/com/crabmod/hotbath/registers/FluidsRegister.java
@@ -0,0 +1,162 @@
+package com.crabmod.hotbath.registers;
+
+import com.crabmod.hotbath.HotBath;
+import com.crabmod.hotbath.fluid_blocks.*;
+import com.crabmod.hotbath.fluid_details.FluidsProperties;
+import com.crabmod.hotbath.fluid_details.HotBathMaterials;
+import net.minecraft.client.resources.model.ModelResourceLocation;
+import net.minecraft.world.level.block.LiquidBlock;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.FlowingFluid;
+import net.minecraft.world.level.material.Fluid;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fluids.ForgeFlowingFluid;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+/** FluidsRegister */
+public class
+FluidsRegister {
+  public static final ModelResourceLocation WATER_STILL_RL =
+      new ModelResourceLocation("block/water_still");
+  public static final ModelResourceLocation WATER_FLOWING_RL =
+      new ModelResourceLocation("block/water_flow");
+  public static final ModelResourceLocation WATER_OVERLAY_RL =
+      new ModelResourceLocation("block/water_overlay");
+
+  public static final DeferredRegister<Fluid> FLUIDS =
+      DeferredRegister.create(ForgeRegistries.FLUIDS, HotBath.MOD_ID);
+
+  // Register the fluid, flowing fluid and fluid block
+
+  public static final RegistryObject<FlowingFluid> HERBAL_BATH_FLUID =
+      FLUIDS.register(
+          "herbal_bath_fluid",
+          () -> new ForgeFlowingFluid.Source(FluidsProperties.HERBAL_BATH_PROPERTIES));
+
+  public static final RegistryObject<FlowingFluid> HERBAL_BATH_FLOWING =
+      FLUIDS.register(
+          "herbal_bath_flowing",
+          () -> new ForgeFlowingFluid.Flowing(FluidsProperties.HERBAL_BATH_PROPERTIES));
+
+  public static final RegistryObject<LiquidBlock> HERBAL_BATH_BLOCK =
+      BlocksRegister.BLOCKS.register(
+          "herbal_bath_block",
+          () ->
+              new HerbalBathBlock(
+                  FluidsRegister.HERBAL_BATH_FLUID,
+                  BlockBehaviour.Properties.of(HotBathMaterials.HOTBATH_MATERIAL)
+                      .noCollission()
+                      .strength(100.0F)
+                      .noDrops()));
+
+  public static final RegistryObject<FlowingFluid> HONEY_BATH_FLUID =
+      FLUIDS.register(
+          "honey_bath_fluid",
+          () -> new ForgeFlowingFluid.Source(FluidsProperties.HONEY_BATH_PROPERTIES));
+
+  public static final RegistryObject<FlowingFluid> HONEY_BATH_FLOWING =
+      FLUIDS.register(
+          "honey_bath_flowing",
+          () -> new ForgeFlowingFluid.Flowing(FluidsProperties.HONEY_BATH_PROPERTIES));
+
+  public static final RegistryObject<LiquidBlock> HONEY_BATH_BLOCK =
+      BlocksRegister.BLOCKS.register(
+          "honey_bath_block",
+          () ->
+              new HoneyBathBlock(
+                  FluidsRegister.HONEY_BATH_FLUID,
+                  BlockBehaviour.Properties.of(HotBathMaterials.HOTBATH_MATERIAL)
+                      .noCollission()
+                      .strength(100.0F)
+                      .noDrops()));
+
+  public static final RegistryObject<FlowingFluid> HOT_WATER_FLUID =
+      FLUIDS.register(
+          "hot_water_fluid",
+          () -> new ForgeFlowingFluid.Source(FluidsProperties.HOT_WATER_PROPERTIES));
+
+  public static final RegistryObject<FlowingFluid> HOT_WATER_FLOWING =
+      FLUIDS.register(
+          "hot_water_flowing",
+          () -> new ForgeFlowingFluid.Flowing(FluidsProperties.HOT_WATER_PROPERTIES));
+
+  public static final RegistryObject<LiquidBlock> HOT_WATER_BLOCK =
+      BlocksRegister.BLOCKS.register(
+          "hot_water_block",
+          () ->
+              new HotWaterBlock(
+                  FluidsRegister.HOT_WATER_FLUID,
+                  BlockBehaviour.Properties.of(HotBathMaterials.HOTBATH_MATERIAL)
+                      .noCollission()
+                      .strength(100.0F)
+                      .noDrops()));
+
+  public static final RegistryObject<FlowingFluid> MILK_BATH_FLUID =
+      FLUIDS.register(
+          "milk_bath_fluid",
+          () -> new ForgeFlowingFluid.Source(FluidsProperties.MILK_BATH_PROPERTIES));
+
+  public static final RegistryObject<FlowingFluid> MILK_BATH_FLOWING =
+      FLUIDS.register(
+          "milk_bath_flowing",
+          () -> new ForgeFlowingFluid.Flowing(FluidsProperties.MILK_BATH_PROPERTIES));
+
+  public static final RegistryObject<LiquidBlock> MILK_BATH_BLOCK =
+      BlocksRegister.BLOCKS.register(
+          "milk_bath_block",
+          () ->
+              new MilkBathBlock(
+                  FluidsRegister.MILK_BATH_FLUID,
+                  BlockBehaviour.Properties.of(HotBathMaterials.HOTBATH_MATERIAL)
+                      .noCollission()
+                      .strength(100.0F)
+                      .noDrops()));
+
+  public static final RegistryObject<FlowingFluid> PEONY_BATH_FLUID =
+      FLUIDS.register(
+          "peony_bath_fluid",
+          () -> new ForgeFlowingFluid.Source(FluidsProperties.PEONY_BATH_PROPERTIES));
+
+  public static final RegistryObject<FlowingFluid> PEONY_BATH_FLOWING =
+      FLUIDS.register(
+          "peony_bath_flowing",
+          () -> new ForgeFlowingFluid.Flowing(FluidsProperties.PEONY_BATH_PROPERTIES));
+
+  public static final RegistryObject<LiquidBlock> PEONY_BATH_BLOCK =
+      BlocksRegister.BLOCKS.register(
+          "peony_bath_block",
+          () ->
+              new PeonyBathBlock(
+                  FluidsRegister.PEONY_BATH_FLUID,
+                  BlockBehaviour.Properties.of(HotBathMaterials.HOTBATH_MATERIAL)
+                      .noCollission()
+                      .strength(100.0F)
+                      .noDrops()));
+
+  public static final RegistryObject<FlowingFluid> ROSE_BATH_FLUID =
+      FLUIDS.register(
+          "rose_bath_fluid",
+          () -> new ForgeFlowingFluid.Source(FluidsProperties.ROSE_BATH_PROPERTIES));
+
+  public static final RegistryObject<FlowingFluid> ROSE_BATH_FLOWING =
+      FLUIDS.register(
+          "rose_bath_flowing",
+          () -> new ForgeFlowingFluid.Flowing(FluidsProperties.ROSE_BATH_PROPERTIES));
+
+  public static final RegistryObject<LiquidBlock> ROSE_BATH_BLOCK =
+      BlocksRegister.BLOCKS.register(
+          "rose_bath_block",
+          () ->
+              new RoseBathBlock(
+                  FluidsRegister.ROSE_BATH_FLUID,
+                  BlockBehaviour.Properties.of(HotBathMaterials.HOTBATH_MATERIAL)
+                      .noCollission()
+                      .strength(100.0F)
+                      .noDrops()));
+
+  public static void register(IEventBus eventBus) {
+    FLUIDS.register(eventBus);
+  }
+}
